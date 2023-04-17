bad1f7954e5cd136cbe10af8ab18c30dcac18d48
Alejandro Barrionuevo Guerrero <alejandrobarrionuevo00@gmail.com>
2023-04-17 16:42:02 +0200

    mod

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,11 +18,13 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
+        'role',
         'name',
         'surname',
         'nick',
         'email',
         'password',
+        
     ];
 
     /**
