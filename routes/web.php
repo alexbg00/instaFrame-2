73c6659f13926f227b363f97dafe3224f8900ab8
Alejandro Barrionuevo Guerrero <alejandrobarrionuevo00@gmail.com>
2023-04-17 15:50:25 +0200

    login

--- routes/web.php
+++ routes/web.php
@@ -34,4 +34,9 @@ Route::get('/', function () {
     die();
 
     return view('welcome');
+
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
