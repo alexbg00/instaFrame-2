07bfefbaf58d9daef66bca13b3d6f8fbc5b2ba64
Alejandro Barrionuevo Guerrero <alejandrobarrionuevo00@gmail.com>
2023-04-18 12:33:56 +0200

    configuracionCompleta

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,7 +12,11 @@ class UserController extends Controller
     }
 
     public function update(Request $request){
+        /* Conseguir el usuario identificado */
+        $user = \Auth::user();
+        $id = $user->id;
 
+        /* validacion del formulario  */
         $validate = $this->validate($request, [
 /*             'name' => 'requiredstring|max:255',
             'surname' => 'required|string|max:255',
@@ -24,11 +28,26 @@ class UserController extends Controller
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users,email,'.\Auth::user()->id],
         ]);
 
-        $id = \Auth::user()->id;
+        /* Recoger datos del formulario */
         $name = $request->input('name');
         $surname = $request->input('surname');
         $nick = $request->input('nick');
         $email = $request->input('email');
+
+        /* Asignar nuevo valores al objeto del usuario */
+        $user->name = $name;
+        $user->surname = $surname;
+        $user->nick = $nick;
+        $user->email = $email;
+
+        /* Ejecutar consulta y cambios en la base de datos */
+        $user->update();
+
+        return redirect()->route('config')->with(['message' => 'Usuario actualizado correctamente']);
+
+
+
+
 
 
 
