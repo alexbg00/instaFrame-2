2d396a04e96fcd76ac15bc1154b933ddce027b97
Alejandro Barrionuevo Guerrero <alejandrobarrionuevo00@gmail.com>
2023-04-19 13:41:20 +0200

    prueba4

--- resources/views/user/config.blade.php
+++ resources/views/user/config.blade.php
@@ -102,6 +102,22 @@
                             </div>
                         </div>
                     </form>
+                    @php
+                    /* descodificar una imagen en base 64 desde la base de datos */
+                    $imagen = Auth::user()->image;
+                @endphp
+
+                    @if (Auth::user()->image)
+                    <div class="row mb-3">
+                        <label for="image_path" class="col-md-4 col-form-label text-md-end"></label>
+                        <div class="col-md-6">
+                            <div class="card" style="width: 18rem;">
+                                <img src="data:image/png;base64,{{ $imagen }}" class="card-img-top" alt="...">
+                            </div>
+                        </div>
+                    </div>
+                @endif
+                
                 </div>
             </div>
         </div>
