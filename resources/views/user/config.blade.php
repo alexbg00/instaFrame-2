0da9a90cc15767e3957edef8a66f3e5c354c5c34
Alejandro Barrionuevo Guerrero <alejandrobarrionuevo00@gmail.com>
2023-04-19 14:02:18 +0200

    avatar

--- resources/views/user/config.blade.php
+++ resources/views/user/config.blade.php
@@ -93,6 +93,21 @@
                                 @enderror
                             </div>
                         </div>
+                        @php
+                        /* descodificar una imagen en base 64 desde la base de datos */
+                        $imagen = Auth::user()->image;
+                        @endphp
+
+                        @if (Auth::user()->image)
+                        <div class="row mb-3">
+                            <label for="image_path" class="col-md-4 col-form-label text-md-end"></label>
+                            <div class="col-md-6">
+                                <div class="card" style="width: 18rem;">
+                                    <img src="data:image/png;base64,{{ $imagen }}" class="card-img-top" alt="...">
+                                </div>
+                            </div>
+                        </div>
+                    @endif
 
                         <div class="row mb-0">
                             <div class="col-md-6 offset-md-4">
@@ -102,22 +117,7 @@
                             </div>
                         </div>
                     </form>
-                    @php
-                    /* descodificar una imagen en base 64 desde la base de datos */
-                    $imagen = Auth::user()->image;
-                @endphp
 
-                    @if (Auth::user()->image)
-                    <div class="row mb-3">
-                        <label for="image_path" class="col-md-4 col-form-label text-md-end"></label>
-                        <div class="col-md-6">
-                            <div class="card" style="width: 18rem;">
-                                <img src="data:image/png;base64,{{ $imagen }}" class="card-img-top" alt="...">
-                            </div>
-                        </div>
-                    </div>
-                @endif
-                
                 </div>
             </div>
         </div>
