2540bb0b68192c2adbc4830998576138e51bdd96
Alejandro Barrionuevo Guerrero <alejandrobarrionuevo00@gmail.com>
2023-05-05 11:38:00 +0200

    maquetacionPerfil y Traduccion

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -2,23 +2,37 @@
 
 
 @section('content')
+<style>
+    .user-info{
+        margin-top: 20px;
+        height: 200px;
+        float: left;
+        padding-top: 20px;
+        padding-left: 15px;
+    }
+</style>
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="data-user">
+            <div class="profile-user">
                 @if ($user->image)
-                <div class="container-avatar">
-                    {{-- imprimir la foto de perfil una sola vez --}}
-                    <img src="data:image/png;base64,{{ $user->image }}">
+                <div class="container-avatar" style="width: 200px; border-radius:900px; overflow:hidden; height:200px; float:left">
+                    <img src="data:image/png;base64,{{ $user->image }}" style="width: 100%">
 
                 </div>
+
                 @endif
+
                 <div class="user-info">
                     <h1>{{ '@'.$user->nick }}</h1>
                     <h2>{{ $user->name.' '.$user->surname }}</h2>
                     <p>{{ 'Se unió: '.\FormatTime::LongTimeFilter($user->created_at) }}</p>
                 </div>
+                <div class="clearfix"></div>
             </div>
+            <hr>
+            <div class="clearfix"></div>
+
             @foreach ($user->images as $image)
                 @include('includes.image', ['image' => $image])
             @endforeach
